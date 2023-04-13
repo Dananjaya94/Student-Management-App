c6388d309da49d6b6aa46c0efefa09865fe12d52
Dananjaya Polwattage <dananjayapolwattage@gmail.com>
2023-04-13 19:38:43 +0530

    Add files via upload
    Deployable database file.

--- DB Creation Script.sql
+++ DB Creation Script.sql
@@ -1,25 +1,18 @@
 CREATE DATABASE StudentDB;
 
 CREATE TABLE Student(
-	
 	id int IDENTITY(1,1) PRIMARY KEY,
-    FirstName varchar(255),
-    LastName varchar(255),
+	FirstName varchar(255),
+	LastName varchar(255),
 	Address varchar(255),
-    City varchar(255)
+	City varchar(255)
 );
 
 insert into Student
-values('Sahan','Gunasekara','Maliban Junction','Rathmalana');
+values('Eric','Brian','SA','CA');
 
 insert into Student
-values('Rakith','Wicks','Maliban Junction','Rathmalana');
+values('Bill','Chen','Guanzhou','China');
 
 insert into Student
-values('Dilshan','Praboda','Maliban Junction','Rathmalana');
-
-insert into Student
-values('Buddhika','Devapura','Maliban Junction','Rathmalana');
-
-insert into Student
-values('Prageeth','Lakshan','Maliban Junction','Rathmalana');
+values('Rick','Martin','Las Vegas','Texas');
